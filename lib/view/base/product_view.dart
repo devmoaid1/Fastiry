394d068ecd620ea48fe4a9a27e957f4574a19543
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2022-08-02 20:39:55 +0200

    favorite screen

--- lib/view/base/product_view.dart
+++ lib/view/base/product_view.dart
@@ -23,68 +23,114 @@ class ProductView extends StatelessWidget {
   final String type;
   final Function(String type) onVegFilterTap;
   final bool showTheme1Restaurant;
-  ProductView({@required this.restaurants, @required this.products, @required this.isRestaurant, this.isScrollable = false,
-    this.shimmerLength = 20, this.padding = const EdgeInsets.all(Dimensions.PADDING_SIZE_SMALL), this.noDataText,
-    this.isCampaign = false, this.inRestaurantPage = false, this.type, this.onVegFilterTap, this.showTheme1Restaurant = false});
+  ProductView(
+      {@required this.restaurants,
+      @required this.products,
+      @required this.isRestaurant,
+      this.isScrollable = false,
+      this.shimmerLength = 20,
+      this.padding = const EdgeInsets.all(Dimensions.PADDING_SIZE_SMALL),
+      this.noDataText,
+      this.isCampaign = false,
+      this.inRestaurantPage = false,
+      this.type,
+      this.onVegFilterTap,
+      this.showTheme1Restaurant = false});
 
   @override
   Widget build(BuildContext context) {
     bool _isNull = true;
     int _length = 0;
-    if(isRestaurant) {
+    if (isRestaurant) {
       _isNull = restaurants == null;
-      if(!_isNull) {
+      if (!_isNull) {
         _length = restaurants.length;
       }
-    }else {
+    } else {
       _isNull = products == null;
-      if(!_isNull) {
+      if (!_isNull) {
         _length = products.length;
       }
     }
 
     return Column(children: [
-
-      type != null ? VegFilterWidget(type: type, onSelected: onVegFilterTap) : SizedBox(),
-
-      !_isNull ? _length > 0 ? GridView.builder(
-        key: UniqueKey(),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisSpacing: Dimensions.PADDING_SIZE_LARGE,
-          mainAxisSpacing: ResponsiveHelper.isDesktop(context) ? Dimensions.PADDING_SIZE_LARGE : 0.01,
-          childAspectRatio: ResponsiveHelper.isDesktop(context) ? 4 : showTheme1Restaurant ? 1.9 : 4,
-          crossAxisCount: ResponsiveHelper.isMobile(context) ? 1 : 2,
-        ),
-        physics: isScrollable ? BouncingScrollPhysics() : NeverScrollableScrollPhysics(),
-        shrinkWrap: isScrollable ? false : true,
-        itemCount: _length,
-        padding: padding,
-        itemBuilder: (context, index) {
-          return showTheme1Restaurant ? RestaurantWidget(restaurant: restaurants[index], index: index, inStore: inRestaurantPage) :  ProductWidget(
-            isRestaurant: isRestaurant, product: isRestaurant ? null : products[index],
-            restaurant: isRestaurant ? restaurants[index] : null, index: index, length: _length, isCampaign: isCampaign,
-            inRestaurant: inRestaurantPage,
-          );
-        },
-      ) : NoDataScreen(
-        text: noDataText != null ? noDataText : isRestaurant ? 'no_restaurant_available'.tr : 'no_food_available'.tr,
-      ) : GridView.builder(
-        key: UniqueKey(),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisSpacing: Dimensions.PADDING_SIZE_LARGE,
-          mainAxisSpacing: ResponsiveHelper.isDesktop(context) ? Dimensions.PADDING_SIZE_LARGE : 0.01,
-          childAspectRatio: ResponsiveHelper.isDesktop(context) ? 4 : showTheme1Restaurant ? 1.9 : 4,
-          crossAxisCount: ResponsiveHelper.isMobile(context) ? 1 : 2,
-        ),
-        physics: isScrollable ? BouncingScrollPhysics() : NeverScrollableScrollPhysics(),
-        shrinkWrap: isScrollable ? false : true,
-        itemCount: shimmerLength,
-        padding: padding,
-        itemBuilder: (context, index) {
-          return showTheme1Restaurant ? RestaurantShimmer(isEnable: _isNull) : ProductShimmer(isEnabled: _isNull, isRestaurant: isRestaurant, hasDivider: index != shimmerLength-1);
-        },
-      ),
-
+      type != null
+          ? VegFilterWidget(type: type, onSelected: onVegFilterTap)
+          : SizedBox(),
+      !_isNull
+          ? _length > 0
+              ? GridView.builder(
+                  key: UniqueKey(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisSpacing: 1,
+                    mainAxisSpacing: 0,
+                    childAspectRatio: ResponsiveHelper.isDesktop(context)
+                        ? 4
+                        : showTheme1Restaurant
+                            ? 1.9
+                            : Dimensions.blockscreenHorizontal * 0.52,
+                    crossAxisCount: ResponsiveHelper.isMobile(context) ? 1 : 2,
+                  ),
+                  physics: isScrollable
+                      ? BouncingScrollPhysics()
+                      : NeverScrollableScrollPhysics(),
+                  shrinkWrap: isScrollable ? false : true,
+                  itemCount: _length,
+                  padding: padding,
+                  itemBuilder: (context, index) {
+                    return showTheme1Restaurant
+                        ? RestaurantWidget(
+                            restaurant: restaurants[index],
+                            index: index,
+                            inStore: inRestaurantPage)
+                        : ProductWidget(
+                            isRestaurant: isRestaurant,
+                            product: isRestaurant ? null : products[index],
+                            restaurant:
+                                isRestaurant ? restaurants[index] : null,
+                            index: index,
+                            length: _length,
+                            isCampaign: isCampaign,
+                            inRestaurant: inRestaurantPage,
+                          );
+                  },
+                )
+              : NoDataScreen(
+                  text: noDataText != null
+                      ? noDataText
+                      : isRestaurant
+                          ? 'no_restaurant_available'.tr
+                          : 'no_food_available'.tr,
+                )
+          : GridView.builder(
+              key: UniqueKey(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisSpacing: Dimensions.PADDING_SIZE_LARGE,
+                mainAxisSpacing: ResponsiveHelper.isDesktop(context)
+                    ? Dimensions.PADDING_SIZE_LARGE
+                    : 0.01,
+                childAspectRatio: ResponsiveHelper.isDesktop(context)
+                    ? 4
+                    : showTheme1Restaurant
+                        ? 1.9
+                        : 4,
+                crossAxisCount: ResponsiveHelper.isMobile(context) ? 1 : 2,
+              ),
+              physics: isScrollable
+                  ? BouncingScrollPhysics()
+                  : NeverScrollableScrollPhysics(),
+              shrinkWrap: isScrollable ? false : true,
+              itemCount: shimmerLength,
+              padding: padding,
+              itemBuilder: (context, index) {
+                return showTheme1Restaurant
+                    ? RestaurantShimmer(isEnable: _isNull)
+                    : ProductShimmer(
+                        isEnabled: _isNull,
+                        isRestaurant: isRestaurant,
+                        hasDivider: index != shimmerLength - 1);
+              },
+            ),
     ]);
   }
 }
